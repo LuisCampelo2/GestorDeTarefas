53d7e32d90073f707d74f396b3b2c706662f3621
Luis Campelo <luisfilipecab@gmail.com>
2024-12-21 12:35:57 -0300

    .

--- scripts/wait_mysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-while ! nc -z $MYSQL_HOST $MYSQL_PORT; do
-  echo "🟡 Waiting for MySQL Database Startup ($MYSQL_HOST $MYSQL_PORT) ..."
-  sleep 60
-done
-
-echo "✅ MySQL Database Started Successfully ($MYSQL_HOST:$MYSQL_PORT)"
-
-
